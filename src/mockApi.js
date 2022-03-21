a91c3e673c7262142c9878f431d15e6755f966a8
q0abd <qiyomovabdulloh3@gmail.com>
2022-03-21 11:16:21 +0500

    start

---
+++ src/mockApi.js
@@ -0,0 +1,13 @@
+/** @format */
+
+let mockYoutubeVideo = [
+	{
+		id: 1,
+		name: 'GITHUB , GIT - Uzbek Tutorial',
+		views: `${1233} views`,
+		channelName: 'Kiyomov Dev',
+		timeAgo: `${12} hours ago`,
+		avatar: true,
+		
+	},
+];
